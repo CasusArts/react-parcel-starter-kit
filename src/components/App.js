37c84eeac2ea70b3d773434b06e5b0699fec3123
Fiodor Gorobet <casusarts@gmail.com>
2019-06-06 13:06:15 +0200

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="inner-big-container">
+        <div>React App</div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
